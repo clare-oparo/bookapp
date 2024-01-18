a6310882e2d76758072d13042ad4ea6633ae058d
Clare <clare.oparo@student.moringaschool.com>
2024-01-18 23:24:32 +0300

    display books on webpage

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function(){
     fetchBooks();
 })
 
+//fetch from Google Books
+
 function fetchBooks() {
     const apiUrl = 'https://www.googleapis.com/books/v1/volumes?q=title';
 
@@ -19,4 +21,29 @@ function fetchBooks() {
             displayBooks(books);
         })
         .catch(error => console.error('Error:', error));
+}
+
+//display books on html
+function displayBooks(books) {
+    const booksDiv = document.getElementById('books');
+    booksDiv.innerHTML = '';
+
+    books.forEach(book => {
+        const bookCard = document.createElement('div');
+        bookCard.className = 'col-md-4 book-card';
+        bookCard.innerHTML = `
+            <div class="card">
+                <img src="${book.coverImageUrl}" class="card-img-top" alt="${book.title}">
+                <div class="card-body">
+                    <h5 class="card-title">${book.title}</h5>
+                    <p class="card-text">Author: ${book.author}</p>
+                    <button class="btn btn-outline-secondary like-btn">Like</button>
+                    <button class="btn btn-success" onclick="addToCart({ id: ${book.id}, title: '${book.title}', author: '${book.author}' })">Add to Cart</button>
+                </div>
+            </div>
+        `;
+        booksDiv.appendChild(bookCard);
+    });
+
+    setupLikeButtons();
 }
