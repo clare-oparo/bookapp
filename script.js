77759e6aaa55f6e9fa9ef2d7636c248e5c01ca09
Clare <clare.oparo@student.moringaschool.com>
2024-01-19 16:15:37 +0300

    corrected displayBooks errors

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 fetch('http://localhost:3000/books')
 .then(response => response.json())
 .then(books => displayBooks(books))
-.catch(error => 'Error occured:', error)
+.catch(error =>console.error('Error occured:', error))
 
-function displayBooks(){
+function displayBooks(books){
     const container = document.getElementById('books-container'); //container holds all the books
     container.innerHTML='';
 
     books.forEach( //all books need their own card to display details, also aesthetics...
         book => {
-            const card = createElement('div'); 
+            const card = document.createElement('div'); 
             card.className = 'card';
             card.style.width = '18rem';
 
             card.innerHTML = `
-            <img src="${book.image}" class="class-img-top" alt="$book.title">
+            <img src="${book.image}" class="class-img-top" alt="${book.title}">
             <div class="card-body">
                 <h4 class="card-title">${book.title}</h4>
                 <h5 class="card-author">by ${book.author}</h5>
